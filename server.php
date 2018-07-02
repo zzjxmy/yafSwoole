abfc6ec69979c3c12e040ad0204cbcf9e565149c
zhangzhijian <zhangzhijian@doweidu.com>
2018-07-02 19:11:33 +0800

    修改

--- server.php
+++ server.php
@@ -7,6 +7,14 @@
  */
 require_once './vendor/autoload.php';
 define('APPLICATION_PATH', dirname(__FILE__));
-\RPC\RpcServer::getInstance()->start();
+\RPC\RpcServer::getInstance()->start(function ($controller, $action, $_instance){
+    ob_start();
+    \Yaf\Dispatcher::getInstance()->autoRender(false);
+    $request     = new \Yaf\Request\Simple("CLI", 'Index', $controller, $action);
+    $_instance->bootstrap()->getDispatcher()->dispatch($request);
+    $response = ob_get_contents();
+    ob_end_clean();
+    return $response;
+});
 
 
